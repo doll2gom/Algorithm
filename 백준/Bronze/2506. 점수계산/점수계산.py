1aadc4245e9b50d4540adadb0f5662e744403749
Shinhye Kim <116542500+doll2gom@users.noreply.github.com>
2023-01-16 17:51:47 +0900

    [Bronze III] Title: 점수계산, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.py
@@ -0,0 +1,11 @@
+T = int(input())
+score = list(map(int,input().split()))
+cnt = 0
+result = 0
+for a in score:
+    if a == 1:
+        cnt += 1
+        result += cnt
+    elif a == 0:
+        cnt = 0
+print(result)
