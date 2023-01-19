4f92e154caeaefe0e3605aff3898df21164a1d69
Shinhye Kim <116542500+doll2gom@users.noreply.github.com>
2023-01-19 20:38:56 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 4648 ms, Memory: 49628 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,17 @@
+N = int(input())
+ent = {}
+lst = []
+
+for n in range(0,N):
+    name, status = input().split()
+    if status == 'enter':
+        ent[name] = status
+    else:
+        del ent[name]
+
+for i in ent:
+    lst.append(i)
+lst.sort()
+lst.reverse()
+
+print(*lst, sep='\n')
