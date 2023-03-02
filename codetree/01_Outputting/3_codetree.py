cf7236d7a14c26cd385cb7b1934dce1ce70c0cfe
doll2gom <shkim_ok@naver.com>
2023-03-02 23:20:39 +0900

    Add codetree

---
+++ codetree/01_Outputting/3_codetree.py
@@ -0,0 +1,13 @@
+#3 변수 출력하기
+
+a, b, c = 7, 23, 30
+print("{0} + {1} = {2}".format(a,b,c))
+
+a, b = 3, 'C'
+print('{0}...{1}'.format(a,b))
+
+a, b = 3, 'C'
+print('{1}!.....!{0}'.format(a,b))
+
+a, b, c = 1, 2, 'C'
+print('{0}->{1}->{2}'.format(a, b, c))
