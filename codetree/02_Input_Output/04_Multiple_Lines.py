c3b472551e1857274aadac10b240559f51ca0df1
doll2gom <shkim_ok@naver.com>
2023-03-08 21:13:03 +0900

    Add codetree 02-4

---
+++ codetree/02_Input_Output/04_Multiple_Lines.py
@@ -0,0 +1,27 @@
+# 1
+a = int(input())
+b = int(input())
+print(a*b)
+
+# 2
+a = int(input())
+b = int(input())
+print(a,b)
+
+# 3
+a = float(input())
+b = float(input())
+print("{0:.2f}".format(a+b))
+
+# 4
+a = float(input())
+b = float(input())
+c = float(input())
+print("{0:.3f}".format(a))
+print("%.3f" % b)
+print(f"{c:.3f}")
+
+# 5
+a, b = input().split()
+c = int(input())
+print(a, b, c)
