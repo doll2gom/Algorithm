1472a9f773e4ec667b52f10a020bd336e5e43fd2
doll2gom <shkim_ok@naver.com>
2023-03-08 21:15:44 +0900

    Add codetree 02-5

---
+++ codetree/02_Input_Output/05_String.py
@@ -0,0 +1,24 @@
+# 1
+c = input()
+print(c)
+
+# 2
+s = input()
+
+print(s)
+
+
+# 3
+c = str(input())
+a = float(input())
+b = float(input())
+
+print(c)
+print(f"{a:.2f}")
+print("%.2f" % b)
+
+# 4
+s = str(input())
+t = str(input())
+print(t)
+print(s)
