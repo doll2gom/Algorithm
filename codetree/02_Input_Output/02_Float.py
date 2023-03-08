34c01148dd3d79718f7f004119ad25c4d2721117
doll2gom <shkim_ok@naver.com>
2023-03-08 21:09:45 +0900

    Add codetree 02-2

---
+++ codetree/02_Input_Output/02_Float.py
@@ -0,0 +1,11 @@
+# 1
+n = float(input())
+print("{0:.2f}".format(n))
+# 2
+n = float(input())
+ft = 30.48
+print("{0:.1f}".format(n * ft))
+
+# 3
+a = float(input())
+print("{0:.2f}".format(a + 1.5))
