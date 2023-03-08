5b91487290f71b9fa44cb695bda8a2d7758faa8b
doll2gom <shkim_ok@naver.com>
2023-03-08 21:11:37 +0900

    Add codetree 02-3

---
+++ codetree/02_Input_Output/03_Input_Between_Blank.py
@@ -0,0 +1,19 @@
+# 1
+arr = input()
+arr = arr.split()
+a = int(arr[0])
+b = int(arr[1])
+
+print(a*b)
+
+# 2
+a, b = input().split()
+print(int(a) + int(b))
+
+# 3
+a, b = input().split()
+print(b, a)
+
+# 4
+a, b = input().split()
+print(a, b, int(a)+int(b))
