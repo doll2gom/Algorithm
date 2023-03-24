616a8a587812d9454567ffab88ae925db3494beb
Shinhye Kim <116542500+doll2gom@users.noreply.github.com>
2023-03-24 23:35:38 +0900

    [D2] Title: 지그재그 숫자, Time: 97 ms, Memory: 43,784 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -0,0 +1,10 @@
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    num = 0
+    for n in range(1, N+1):
+        if n%2 != 0:
+            num += n
+        else:
+            num -= n
+    print(f'#{t} {num}')
